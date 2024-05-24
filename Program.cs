a4e9477dd57b29f6f175e80970c4a0c82c954d6b
eldar23451 <liyev8024@gmail.com>
2024-05-24 17:51:10 +0400

    task file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel.Design;
+
+int BornYear(int year, int age)
+{
+    Console.WriteLine("please enter your born year");
+    return age;
+    if (age > 0)
+    {
+       Console.WriteLine("yas sifirdan kicik ola bilmez");
+
+    }
+    else
+    {
+        Console.WriteLine("yas sifira beraberdir");
+    }
+    Console.WriteLine(BornYear(2024, 2005));
+    
